6e554957c7633b705b2d97d1b5cd2f81c17c6f3b
Dylan McNamee <dylan.mcnamee@gmail.com>
2015-05-04 16:40:50 -0700

    fixed movement

--- base.py
+++ base.py
@@ -1,49 +1,58 @@
 import cmd
 from room import get_room
-from pprint import pprint
+import pprint
+import json
 
+# to understand how the commands get processed, see http://pymotw.com/2/cmd/
 
 class Game(cmd.Cmd):
-	def __init__(self):
-		cmd.Cmd.__init__(self)
+    def __init__(self):
+        cmd.Cmd.__init__(self)
 
-		self.loc = get_room(1)
-		self.look()
+        self.loc = get_room(1)
+        self.look()
+        self.pp = pprint.PrettyPrinter(indent=4,depth=6)
 
-	def move(self, dir):
-		pprint(self.loc)
-		newsroom = self.loc._neighbors(dir)
-		if newsroom is None:
-			print("You stumble into a wall")
-		else:
-			self.loc = get_room(newsroom)
+    def move(self, dir):
+        # print "moving " + dir
+        newsroom = self.loc.getNeighbor(dir)
+        # print "found " + str(newsroom)
+        if newsroom is None:
+            print("You stumble into a wall")
+        else:
+            self.loc = get_room(newsroom)
+        # print "next room " + str(get_room(newsroom))
 
-	def look(self):
-		print(self.loc.name)
-		print("")
-		print(self.loc.description)
+    def look(self):
+        print(self.loc.name)
+        print("")
+        print(self.loc.description)
 
-	def do_n(self, args):
-		"""Go north"""
-		self.move('n')
+    def do_n(self, args):
+        """Go north"""
+        self.move('n')
 
-	def do_e(self, args):
-		"""Go east"""
-		self.move('e')
+    def do_e(self, args):
+        """Go east"""
+        self.move('e')
 
-	def do_s(self, args):
-		"""Go south"""
-		self.move('s')
+    def do_s(self, args):
+        """Go south"""
+        self.move('s')
 
-	def do_w(self, args):
-		"""Go west"""
-		self.move('w')
+    def do_w(self, args):
+        """Go west"""
+        self.move('w')
 
-	def do_quit(self, args):
-		"""Leaves the game"""
-		print("Thank you for playing")
-		return True
+    def do_quit(self, args):
+        """Leaves the game"""
+        print("Thank you for playing")
+        return True
+
+    def do_look(self, args):
+        print(json.dumps(self.loc.__dict__))
+        #self.look()
 
 if __name__ == "__main__":
-	g = Game()
-	g.cmdloop()
+    g = Game()
+    g.cmdloop()
--- room.py
+++ room.py
@@ -1,35 +1,47 @@
 import json
 
 def get_room(id):
-	ret = None
-	with open(str(id)+".json", "r") as r:
-		jsontext = r.read()
-		d = json.loads(jsontext)
-		d['id'] = id
-		ret = Room (**d)
-	return ret
+    ret = None
+    with open(str(id)+".json", "r") as r:
+        jsontext = r.read()
+        d = json.loads(jsontext)
+        d['id'] = id
+        ret = Room (**d)
+        print ret
+    return ret
 
 class Room():
-	def __init__(self, id=0, name= "Room #1", description= "An empty room", neighbors={}):
-		self.id = id
-		self.name = name
-		self.description = description
-		self.neighbors = neighbors
+    def __init__(self, id=0, name= "Room #1", description= "An empty room", neighbors={}):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.neighbors = neighbors
 
-	def _neighbor(self, direction):
-		if direction in self.neighbors:
-			return self.neighbors[direction]
-		else:
-			return None
+    def __repr__(self):
+        ret = self.description + '...' + self.name + '\n'
+        exits = ""
+        anyExits = False
+        for d in self.neighbors:
+            anyExits = True
+            exits += d + ', '
+        if anyExits:
+            ret = ret + "You see exits to the " + exits
+        return ret
 
-	def north(self):
-		return self._neighbor('n')
+    def getNeighbor(self, direction):
+        if direction in self.neighbors:
+            return self.neighbors[direction]
+        else:
+            return None
 
-	def east(self):
-		return self._neighbor('e')
+    def north(self):
+        return self._neighbor('n')
 
-	def south(self):
-		return self._neighbor('s')
+    def east(self):
+        return self._neighbor('e')
 
-	def west(self):
-		return self._neighbor('w')
+    def south(self):
+        return self._neighbor('s')
+
+    def west(self):
+        return self._neighbor('w')
